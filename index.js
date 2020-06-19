53cf7c831b943951cfb510506e485265bf991896
Jason Huang <jasonhuang20035@gmail.com>
2020-06-18 20:52:35 -0400

    Add Basic Functionality

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 // Load dependencies
 const Discord = require("discord.js");
-const { token } = require("./config");
+const config = require("./config.json");
 
 // Create client instance
 const client = new Discord.Client();
-const PREFIX = '!';
 
 // Log once client is ready
 client.once("ready", () => {
   console.log("Ready!");
 });
 
-client.on('message', async message => {
-  if (message.content.startsWith(PREFIX)) {
-    const input = message.content.slice(PREFIX.length).split(' ');
-    
+client.on("message", async (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  const args = message.content.slice(config.prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  console.log(args);
+  console.log(command);
+  if (command == "ping") {
+    message.channel.send("Pong!");
   }
-})
+});
 
 // Login with app token
-client.login(token);
+client.login(config.token);
